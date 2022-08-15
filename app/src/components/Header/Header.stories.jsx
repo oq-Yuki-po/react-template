8ec7e46687cc25b1240dfd264735032bdb101605
oq-Yuki-po <o.yukinko0715@gmail.com>
2022-08-15 05:31:50 +0000

    modify story bok

--- app/src/components/Header/Header.stories.jsx
+++ app/src/components/Header/Header.stories.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Header from './Header';
+
 
 export default {
     title: 'Example/Header',
     component: Header,
 };
 
-const Template = () => <Header />;
+const Template = () => <BrowserRouter><Header /></BrowserRouter>;
 
 export const Default = Template.bind({});
